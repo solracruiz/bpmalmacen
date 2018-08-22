0abce19f5fb09e6b6adaf2792af2050ab996bd5f
Carlos Ruiz <solrac_ruiz@hotmail.com>
2018-08-22 17:29:21 -0600

    requi2

--- Requisiciones.Designer.cs
+++ Requisiciones.Designer.cs
@@ -28,11 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btagregar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btagregar
+            // 
+            this.btagregar.Location = new System.Drawing.Point(631, 221);
+            this.btagregar.Name = "btagregar";
+            this.btagregar.Size = new System.Drawing.Size(75, 24);
+            this.btagregar.TabIndex = 0;
+            this.btagregar.Text = "Agregar";
+            this.btagregar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(676, 164);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Requisiciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btagregar);
+            this.Name = "Requisiciones";
             this.Text = "Requisiciones";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btagregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
